d4171f384defd7955631e067a5eadb97a96d9e33
mykhailo-lutsiv <mykhailo-lutsiv@users.noreply.github.com>
2023-01-15 19:43:53 +0000

    deploy: 3008a5b4770a9b99187cb367b02deb882191c5dc

---
+++ _next/static/75JaySQSifZIQo06ZYreF/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/589c0f0187ce6836.css","static/chunks/pages/index-7c9bea4c19334a35.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
